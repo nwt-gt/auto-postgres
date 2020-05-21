56ec46bc4b80984d0b2a845062ec743180a7a4f2
nwt <weetong@dsaid.gov.sg>
2020-05-21 17:19:15 +0000

    changed runscript

--- runscript.sh
+++ runscript.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-docker volume rm autotest_12-config
+#docker volume rm autotest_12-config
+echo "Cleaning data"
 sudo rm -rf data
+echo "Creating data"
 mkdir -p data/12
 mkdir -p data/9.6
+echo "Done"
 #mkdir -p data/9.6-config
 #mkdir -p data/12-config
